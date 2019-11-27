3d0969d271fc16ec469cb60a49bf08cfeb7f0dc6
Zhanzhou Ta <tazhanzhou@outlook.com>
2019-11-27 18:39:17 -0500

    added 3 new tables

---
+++ Class4DB/Classes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ClassName] VARCHAR(50) NOT NULL, 
+    [ClassNumber] INT NOT NULL 
+)
---
+++ Class4DB/Students.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StudentId] INT NOT NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [gender] NCHAR(10) NOT NULL, 
+    [BoD] DATE NOT NULL
+)
